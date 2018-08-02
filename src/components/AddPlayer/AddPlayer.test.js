8fb078015aa3aade63466d3a55cc400fa31195c3
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-02 12:59:16 +0200

    Add AddPlayer directory with AddPlayer.js, AddPlayer.css and AddPlayer.test.js files

---
+++ src/components/AddPlayer/AddPlayer.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import AddPlayer from './AddPlayer';
+
+it('renders without crashing', () => {
+  shallow(<AddPlayer/>)
+})
+it('renders with correct name', () => {
+  const onPlayerAdd = jest.fn();
+  const addPlayerComponent = mount(<AddPlayer onPlayerAdd={onPlayerAdd} />);
+
+  const nameInput = addPlayerComponent.find('input').first().getDOMNode();
+
+  nameInput.value = 'Bobby Draper';
+  const form = addPlayerComponent.find('form');
+  form.simulate('submit');
+
+  expect(onPlayerAdd).toBeCalledWith('Bobby Draper');
+})
