6d155301080270f39de0b4546438fb54acce9225
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-02 12:59:57 +0200

    Add PlayersList directory with PlayersList.css, PlayersList.js and PlayersList.test.js files

---
+++ src/components/PlayersList/PlayersList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Player from '../Player/Player';
+import './PlayersList.css';
+
+const PlayersList = (props) => (
+  
+  <ul className="PlayersList">
+  <h1 className="Player_table">Player table</h1>
+    {props.players.map((player, i) => (
+      <Player
+          key={i}
+          name={player.name}
+          score={player.score}
+          onPlayerScoreChange={(points) => props.onScoreUpdate(i, points)}
+      />)
+    )}
+    </ul>
+);
+
+export default PlayersList;
---
+++ src/components/PlayersList/PlayersList.test.js
@@ -0,0 +1,51 @@
+import PlayersList from './PlayersList';
+import Player from '../Player/Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  shallow(<PlayersList players={[]}/>);
+});
+
+it('renders correct number of players', () => {
+  const players = [
+    {
+        name: 'Naomi Nagata',
+        score: 5
+    },
+    {
+        name: 'James Holden',
+        score: 0
+    }
+  ]
+  const playerComponent = shallow(<PlayersList players={players} />);
+  console.log(playerComponent.debug());
+  const expectedPlayersNumber = playerComponent.find(Player).length;
+
+  expect(expectedPlayersNumber).toEqual(2);
+});
+
+it('should call on ScoreUpdate', () => {
+  const players = [
+    {
+        name: 'Naomi Nagata',
+        score: 5
+    },
+    {
+        name: 'James Holden',
+        score: 0
+    }
+  ]
+  const mockedOnScoreUpdate = jest.fn();
+
+  const playerComponent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+
+  const firstPlayer = playerComponent.find(Player).first();
+  const secondPlayer = playerComponent.find(Player).last();
+
+  const onPlayerScoreChange = secondPlayer.prop('onPlayerScoreChange');
+
+  onPlayerScoreChange(5);
+
+  expect(mockedOnScoreUpdate).toBeCalledWith(1, 5);
+});
