375bf9e1e776f7f5ec75b6bb49fa4073cf1ca3aa
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-02 20:38:27 +0200

    Update App.test.js with test for remove button functionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import PlayersList from '../src/components/PlayersList/PlayersList';
 import AddPlayer from '../src/components/AddPlayer/AddPlayer';
 import App from './App';
+const testPlayers = [
+  {
+      name: 'Naomi Nagata',
+      score: 5
+  },
+  {
+      name: 'James Holden',
+      score: 0
+  },
+  {
+    name: 'Amos Burton',
+    score: 3
+  },
+  {
+    name: 'Clarissa Mao',
+    score: 2
+  }
+]
 
 it('renders without crashing', () => {
   shallow(<App />);
@@ -40,4 +58,35 @@ it('should add a new player to the app', () => {
     expect(players.length).toEqual(1);
     expect(players[0].name).toEqual('Bobby Draper');
     expect(players[0].score).toEqual(0);
-})
+});
+
+it ('should remove a player or players from the app', () => {
+    const appComponent = shallow(<App />);
+
+    const players = testPlayers;
+    appComponent.setState({ players });
+
+    const onPlayerRemove = appComponent.find(PlayersList).prop('onPlayerRemove');
+
+    onPlayerRemove(1);
+
+    const playersAfterUpdate = appComponent.state('players');
+
+    expect(playersAfterUpdate.length).toEqual(testPlayers.length -1)
+});
+
+it ('should remove a player when remove button is clicked', () => {
+    const appComponent = mount(<App />);
+
+    const players = testPlayers;
+    appComponent.setState({ players });
+
+    const removeButton = appComponent.find('.Player__button-remove').at(0);
+    console.log(removeButton);
+    removeButton.simulate('click');
+
+    const playersAfterUpdate = appComponent.state('players');
+
+    expect(playersAfterUpdate.length).toEqual(testPlayers.length -1 );
+
+});
