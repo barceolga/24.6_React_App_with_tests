b7ff01bd8e66c9419ad45682a2aedd374ec0b931
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-03 13:33:45 +0200

    Update Player.js with icons

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Player.css';
 
 const Player = (props) => (
   <li className="Player">
+    <span className="Player__trophy"><FontAwesomeIcon icon="trophy" className="Icon" /></span>
     <span className="Player__name">{props.name}</span>
-    <span className="Player__score">{props.score}</span>
-    <span className="Player__button" onClick={() => props.onPlayerScoreChange(1)}>+</span>
-    <span className="Player__button" onClick={() => props.onPlayerScoreChange(-1)}>-</span>
-    <span className="Player__button-remove" onClick={() => props.onPlayerRemove()}>X</span>
+    <span className={`Player__score ${props.score>=0?'positive':'negative'}`}>{props.score}</span>
+    <span className="Player__button" onClick={() => props.onPlayerScoreChange(1)}><FontAwesomeIcon icon="plus" className="Icon" /></span>
+    <span className="Player__button" onClick={() => props.onPlayerScoreChange(-1)}><FontAwesomeIcon icon="minus" className="Icon" /></span>
+    <span className="Player__button-remove" onClick={() => props.onPlayerRemove()}><FontAwesomeIcon icon="trash-alt" className="Icon" /></span>
   </li>
 );
 
