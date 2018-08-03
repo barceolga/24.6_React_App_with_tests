2190f992665c4e7cbcd3840fa19b3062f412e94f
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-03 13:34:54 +0200

    Update PlayersList.js content with new button for classifying players

--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -5,7 +5,15 @@ import './PlayersList.css';
 const PlayersList = (props) => (
 
   <ul className="PlayersList">
-  <h1 className="Player_table">Player table</h1>
+  <button className="PlayersList__classification" onClick={() => props.sortResults()}>Classify</button>
+  <h1 className="Player__table">Scoreboard</h1>
+  <li className="Player__table-title">
+      <span className="Heading">Player name</span>
+      <span className="Heading">Player score</span>
+      <span className="Heading">Add points</span>
+      <span className="Heading">Remove points</span>
+      <span className="Heading">Remove player</span>
+  </li>
     {props.players.map((player, i) => (
       <Player
           key={i}
@@ -13,7 +21,7 @@ const PlayersList = (props) => (
           score={player.score}
           onPlayerScoreChange={(points) => props.onScoreUpdate(i, points)}
           onPlayerRemove={() => props.onPlayerRemove(i)}
-      />)
+     />)
     )}
     </ul>
 );
