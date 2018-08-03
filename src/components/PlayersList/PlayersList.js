d0fe20cf4315ef8ea767820a1717f2804621887e
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-03 16:19:37 +0200

    Update PlayersList component

--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -8,15 +8,17 @@ const PlayersList = (props) => (
   <button className="PlayersList__classification" onClick={() => props.sortResults()}>Classify</button>
   <h1 className="Player__table">Scoreboard</h1>
   <li className="Player__table-title">
-      <span className="Heading">Player name</span>
-      <span className="Heading">Player score</span>
-      <span className="Heading">Add points</span>
-      <span className="Heading">Remove points</span>
-      <span className="Heading">Remove player</span>
+      <div className="Heading"> Ranking </div>
+      <div className="Heading">Player name</div>
+      <div className="Heading">Player score</div>
+      <div className="Heading">Add points</div>
+      <div className="Heading">Remove points</div>
+      <div className="Heading">Remove player</div>
   </li>
     {props.players.map((player, i) => (
       <Player
           key={i}
+          id={i}
           name={player.name}
           score={player.score}
           onPlayerScoreChange={(points) => props.onScoreUpdate(i, points)}
