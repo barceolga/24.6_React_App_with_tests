e08cedaa1fc5af07ff3f469e386a9a65bc78b6a2
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-08 14:10:42 +0200

    Update content for Player.js

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,10 +1,12 @@
-import React from 'react';
+// eslint-disable-next-line
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Player.css';
 
 const Player = (props) => (
   <li className="Player">
-    <div className={`Player__trophy ${props.id === 0?'visible':'invisible'}`}><FontAwesomeIcon icon="trophy" className="Icon" /><span>{props.id}.</span></div>
+    <div className="Player__trophy"><FontAwesomeIcon icon="trophy" className={`Icon ${props.id === 0?'visible':'invisible'}`} /><span>{props.id +1}.</span></div>
     <div className="Player__name">{props.name}</div>
     <div className={`Player__score ${props.score>=0?'positive':'negative'}`}>{props.score}</div>
     <div className="Player__button" onClick={() => props.onPlayerScoreChange(1)}><FontAwesomeIcon icon="plus" className="Icon" /></div>
